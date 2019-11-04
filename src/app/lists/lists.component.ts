f3703137f2310683aba10aa50da1d94fcf52aaec
Elizabeth Rodriguez <elizabeth.rodriguez@konfio.mx>
2019-11-04 09:31:40 -0600

    Add graph report

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from './canvasjs.min';
+
 
 @Component({
   selector: 'app-lists',
@@ -10,6 +12,41 @@ export class ListsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let chart = new CanvasJS.Chart("chartContainer", {
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: true,
+      title:{
+        text: "Reporte anual de incidencias"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: {y} (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 450, name: "Enero" },
+          { y: 120, name: "Febrero" },
+          { y: 300, name: "Marzo" },
+          { y: 800, name: "Abril" },
+          { y: 150, name: "Mayo" },
+          { y: 150, name: "Junio"},
+          { y: 250, name: "Julio" },
+          { y: 450, name: "Agosto" },
+          { y: 120, name: "Septiembre" },
+          { y: 300, name: "Octubre" },
+          { y: 800, name: "Noviembre" },
+          { y: 150, name: "Diciembre" }
+        ]
+      }]
+    });
+      
+    chart.render();
+
+
   }
 
 }
+
+
+ 
